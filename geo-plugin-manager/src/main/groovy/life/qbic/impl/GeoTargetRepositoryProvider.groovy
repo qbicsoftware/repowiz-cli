bf1927940270d043d9a41f6d3252520658c6101d
jenniferboedker <jennifer.boedker@student.uni-tuebingen.de>
2020-03-20 17:53:01 +0100

    add geo plugin classes

---
+++ geo-plugin-manager/src/main/groovy/life/qbic/impl/GeoTargetRepositoryProvider.groovy
@@ -0,0 +1,12 @@
+package life.qbic.impl
+
+import life.qbic.repowiz.finalise.api.SubmissionManager
+import life.qbic.repowiz.finalise.spi.TargetRepositoryProvider
+
+class GeoTargetRepositoryProvider implements TargetRepositoryProvider{
+
+    @Override
+    SubmissionManager create() {
+        return null
+    }
+}
