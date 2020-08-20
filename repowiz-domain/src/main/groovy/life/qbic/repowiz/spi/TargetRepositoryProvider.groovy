ecd16f6c57f700ea8b5c9f279f6c42fdecd17444
jnnfr <jennifer.boedker@qbic.uni-tuebingen.de>
2020-08-20 13:50:33 +0200

    rewrite method description
    Co-authored-by: Tobias Koch <KochTobi@users.noreply.github.com>

--- repowiz-domain/src/main/groovy/life/qbic/repowiz/spi/TargetRepositoryProvider.groovy
+++ repowiz-domain/src/main/groovy/life/qbic/repowiz/spi/TargetRepositoryProvider.groovy
@@ -5,8 +5,8 @@ import life.qbic.repowiz.Repository
 /**
  * SPI interface that determines the functions a repository provider must have
  *
- * This class defines how a repository provider should look like in order to be supported by RepoWiz. This class needs to
- * be implemented by all providers that should be found by the system!
+ * This class defines how a repository provider should look like in order to be supported by RepoWiz. 
+ * This class has to be implemented by all repository providers in order for them to be found by RepoWiz!
  *
  *  @since: 1.0.0
  *  @author: Jennifer Bödker
